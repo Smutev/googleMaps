fa1e6b1f8c48972d789fc1d9d1aa67d9092f24ae
Александр Мутев <ma@ithillel.ua>
2020-09-02 10:17:46 +0300

    Add slider

--- googleMaps/src/app/components/chart/chart.component.ts
+++ googleMaps/src/app/components/chart/chart.component.ts
@@ -1,7 +1,8 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import * as moment from 'moment';
 import { EventsService } from '../../services/events.service';
 import { ChartEvent } from '../../shared/interfaces';
+import cloneDeep from 'lodash.clonedeep';
 
 @Component({
   selector: 'app-chart',
@@ -10,6 +11,28 @@ import { ChartEvent } from '../../shared/interfaces';
 })
 export class ChartComponent implements OnInit {
   @Output() showInfo = new EventEmitter<any>();
+  @ViewChild('inputLeft') public inputLeft;
+  @ViewChild('thumbLeft') public thumbLeft;
+  @ViewChild('inputRight') public inputRight;
+  @ViewChild('thumbRight') public thumbRight;
+  @ViewChild('range') public range;
+
+
+  public startOfTheDay = moment()
+    .utc()
+    .local()
+    .startOf('day');
+
+  public copiedCurrentEvent: ChartEvent;
+  public currentEvent: ChartEvent;
+  public previousEvent: ChartEvent;
+  public nextEvent: ChartEvent;
+  public copiedNextEvent: ChartEvent;
+  public copiedPreviousEvent: ChartEvent;
+
+  // public thumbLeft;
+  // public thumbRight;
+  // public range;
 
   public cellWidthInPx = 50;
   public Math: any;
@@ -65,28 +88,157 @@ export class ChartComponent implements OnInit {
   }
 
   public getChartEvents(): void {
-    this.eventsService.getEvents().subscribe((response) => {
+    this.eventsService.getEvents().subscribe((response: ChartEvent[]) => {
       if (response) {
-      const startOfTheDay = moment()
-          .utc()
-          .local()
-          .startOf('day');
+        // const startOfTheDay = moment()
+        //   .utc()
+        //   .local()
+        //   .startOf('day');
 
-        this.chartEvents = response.map(chartEvent => {
-          chartEvent.spent_minutes = chartEvent.end_time.diff(chartEvent.start_time, 'minutes');
-          chartEvent.minutes_from_day_start = chartEvent.start_time.diff(startOfTheDay, 'minutes');
-          chartEvent.row_number = this.rows.findIndex(row => row.value === chartEvent.type);
+        this.chartEvents = response.map((chartEvent) => {
+          chartEvent.spent_minutes = chartEvent.end_time.diff(
+            chartEvent.start_time,
+            'minutes',
+          );
+          chartEvent.minutes_from_day_start = chartEvent.start_time.diff(
+            this.startOfTheDay,
+            'minutes',
+          );
+          chartEvent.row_number = this.rows.findIndex(
+            (row) => row.value === chartEvent.type,
+          );
           return chartEvent;
         });
       }
     });
   }
 
-  public showAdditionalInformation(chartEvent): void {
-    console.log(chartEvent);
+  public showAdditionalInformation(
+    chartEvent: ChartEvent,
+    index: number,
+  ): void {
+    this.currentEvent = chartEvent;
+    this.copiedCurrentEvent = cloneDeep(this.currentEvent);
 
+    if (index !== this.chartEvents.length - 1) {
+      this.nextEvent = this.chartEvents[index + 1];
+      this.copiedNextEvent = cloneDeep(this.nextEvent);
+    }
+
+    if (index !== 0) {
+      this.previousEvent = this.chartEvents[index - 1];
+      this.copiedPreviousEvent = cloneDeep(this.previousEvent);
+    }
 
     // TODO Здесь будет емит события на показ нижней части
     // this.showInfo.emit(chartEvent)
+  }
+
+  public save(): void {}
+
+  public changeRange(e, isMax?: boolean): void {
+    if (!this.currentEvent) {
+      return;
+    }
+
+    const value = +e.target.value;
+    const difference = this.currentEvent.spent_minutes - value;
+////////////////
+//     const min = parseInt(e.target.min);
+//     const max = parseInt(e.target.max);
+//
+//     const value = this.Math.min(parseInt(e.target.value), parseInt(this.inputRight.nativeElement.value));
+//     const percent = ((value - min) / (max - min) * 100);
+//
+//     this.thumbLeft = percent;
+//     this.range = percent;
+//
+//     const difference = this.currentEvent.spent_minutes - value;
+    /////////////
+
+    if (isMax) {
+      const min = parseInt(e.target.min);
+      const max = parseInt(e.target.max);
+
+      const Nvalue = this.Math.max(parseInt(e.target.value), parseInt(this.inputLeft.nativeElement.value) + 1);
+      const percent = ((Nvalue - min) / (max - min)) * 100;
+
+      this.thumbRight.nativeElement.style.right = (100 - percent) + '%';
+      this.range.nativeElement.style.right = (100 - percent) + '%';
+
+      // const difference = this.currentEvent.spent_minutes - value;
+
+
+
+
+      this.currentEvent.spent_minutes = this.copiedCurrentEvent.spent_minutes;
+      this.currentEvent.end_time = moment(this.copiedCurrentEvent.end_time);
+
+      if (difference > 0) {
+        this.currentEvent.end_time.subtract(difference, 'minutes');
+      }
+
+      if (
+        this.currentEvent.end_time.diff(
+          this.currentEvent.start_time,
+          'minutes',
+        ) > 0
+      ) {
+        this.currentEvent.spent_minutes = this.currentEvent.end_time.diff(
+          this.currentEvent.start_time,
+          'minutes',
+        );
+      } else {
+        this.currentEvent.spent_minutes = 0;
+      }
+
+      this.nextEvent.start_time = moment(this.copiedNextEvent.start_time);
+      if (difference > 0) {
+        this.nextEvent.start_time.subtract(difference, 'minutes');
+      } else {
+        this.nextEvent.start_time.subtract(0, 'minutes');
+      }
+      this.recountTimeSpaces(this.nextEvent);
+      this.recountTimeSpaces(this.previousEvent);
+    } else {
+      const min = parseInt(e.target.min);
+      const max = parseInt(e.target.max);
+
+      const Nvalue = this.Math.min(parseInt(e.target.value), parseInt(this.inputRight.nativeElement.value) - 1);
+      const percent = ((Nvalue - min) / (max - min)) * 100;
+
+      this.thumbLeft.nativeElement.style.left = percent + '%';
+      this.range.nativeElement.style.left = percent + '%';
+
+
+
+
+
+
+      this.currentEvent.end_time = moment(this.copiedCurrentEvent.end_time);
+      this.currentEvent.start_time = moment(this.copiedCurrentEvent.start_time);
+      this.currentEvent.start_time.add(value, 'minutes');
+      this.recountTimeSpaces(this.currentEvent);
+
+      this.previousEvent.end_time = moment(this.copiedPreviousEvent.end_time);
+      this.previousEvent.end_time.add(value, 'minutes');
+      this.recountTimeSpaces(this.previousEvent);
+
+      this.nextEvent.start_time = moment(this.copiedNextEvent.start_time);
+
+      this.nextEvent.start_time.subtract(0, 'minutes');
+      this.recountTimeSpaces(this.nextEvent);
+    }
+  }
+
+  public recountTimeSpaces(chartEvent: ChartEvent) {
+    chartEvent.spent_minutes = chartEvent.end_time.diff(
+      chartEvent.start_time,
+      'minutes',
+    );
+    chartEvent.minutes_from_day_start = chartEvent.start_time.diff(
+      this.startOfTheDay,
+      'minutes',
+    );
   }
 }
